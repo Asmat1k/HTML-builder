334fbee542abf1c17fc22c46c620328d139da901
Asmat1k <dobrovti@mail.ru>
2023-05-08 22:04:02 +0300

    fix: 04-copy-directory fix for delete files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,6 +8,16 @@ fs.mkdir(path.join(__dirname,'file-copy'), { recursive: true }, err => {
 
 fs.readdir(path.join(__dirname, 'files'), (err, origFiles) => {
   if(err) throw `Ошибка в открытии файла${err}`;
+  fs.readdir(path.join(__dirname, 'file-copy'), (err, copyFiles) => {
+    if(err) throw `Ошибка в чтении папки-копии ${err}`;
+    copyFiles.forEach(file => {
+      if (!origFiles.includes(file)) {
+        fs.rm(path.join(__dirname, 'file-copy', file), (err) => {
+          if(err) throw `Ошибка в удалении файла из папки-копии${err}`;
+        });
+      }
+    });
+  });
   for(let i = 0; i < origFiles.length; i += 1) {
     fs.copyFile(path.join(__dirname, 'files', path.basename(origFiles[i])), path.resolve(__dirname, './file-copy', origFiles[i]), (err) => {
       if(err) throw `Ошибка в копировании файл ${err}`;
